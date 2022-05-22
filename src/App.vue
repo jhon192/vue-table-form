23fd9f5fa20aefb728411571d4ead1584781855c
jhon192 <jhon19.2@outlook.com>
2022-05-21 22:15:00 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="p-10">
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead
+          class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+        >
+          <tr>
+            <th scope="col" class="px-6 py-3">Image</th>
+            <th scope="col" class="px-6 py-3">First name</th>
+            <th scope="col" class="px-6 py-3">lastname</th>
+            <th scope="col" class="px-6 py-3">Email</th>
+          </tr>
+        </thead>
+        <tbody v-for="dato in datos" :key="dato.id">
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <td class="px-6 py-4">
+              <img :src="dato.avatar" alt="klk" />
+            </td>
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">{{ dato.first_name }}</th>
+            <td class="px-6 py-4">{{ dato.last_name }}</td>
+            <td class="px-6 py-4">{{ dato.email }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      datos: [],
+    };
+  },
+  async mounted() {
+    const res = (await fetch("https://reqres.in/api/users?page=2")).json();
+    const data = await res;
+    data.data.map((x) => {
+      this.datos.push(x);
+    });
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
